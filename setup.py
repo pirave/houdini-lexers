536d8375fa1146538f403bc4d5ec9c452ea2f158
Pirave Eahalaivan <pirave@gmail.com>
2017-04-04 15:38:51 -0400

    Updating to version 1.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "houdini-lexers",
-    version = "1.1",
+    version = "1.2",
     packages = find_packages(),
     description = 'This package contains the pygments lexer for the vex language used in Houdini',
     author = 'SideFX Software Inc.',
     author_email = 'pirave@sidefx.com',
     url = 'https://github.com/pirave/houdini-lexers',
-    download_url = 'https://github.com/pirave/houdini-lexers/tarball/1.1',
+    download_url = 'https://github.com/pirave/houdini-lexers/tarball/1.2',
     install_requires="Pygments",
     entry_points="""
         [pygments.lexers]
